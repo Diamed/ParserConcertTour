7ab936198d81d4ae2e3fda3374b96311f82d20c4
diamed <apollondiamed@gmail.com>
2016-06-13 22:29:36 +0500

    Deleted unnecessary directory

--- ParserConcertTour/MainWindow.xaml.cs
+++ ParserConcertTour/MainWindow.xaml.cs
@@ -1,23 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using HtmlAgilityPack;
 using System.Net;
-using System.IO;
-using System.Xml.Serialization;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
 
 namespace ParserConcertTour
